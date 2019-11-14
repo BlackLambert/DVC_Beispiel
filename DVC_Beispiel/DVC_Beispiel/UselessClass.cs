5fda92af2b7cf7facead1639270de9e0d1724195
Sebastian Baier HomePC <sebastian.baier93@hotmail.de>
2019-11-14 03:34:06 +0100

    Adds UselessClass. Adds entry to .gitignore

---
+++ DVC_Beispiel/DVC_Beispiel/UselessClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DVC_Beispiel
+{
+	class UselessClass
+	{
+		public void UselessMethod()
+		{
+			Console.WriteLine("Why does this exist?");
+		}
+	}
+}
